eda3649d8ddba493c3d4b840d942d06d47dc225e
Vadim Voloschuk <vvoloschuk@irbis-tech.ru>
2023-09-28 19:41:49 +0300

    add pyvis

---
+++ pyvizmeow.py
@@ -0,0 +1,11 @@
+import pyvis
+from pyvis.network import Network
+
+g = Network()
+g.add_nodes([1,2,3], value=[10, 100, 400],
+                         title=['I am node 1', 'node 2 here', 'and im node 3'],
+                         x=[21.4, 54.2, 11.2],
+                         y=[100.2, 23.54, 32.1],
+                         label=['NODE 1', 'NODE 2', 'NODE 3'],
+                         color=['#00ff1e', '#162347', '#dd4b39'])
+g.save_graph('nx.html')
--- server/app/views.py
+++ server/app/views.py
@@ -1,9 +1,21 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+from pyvis.network import Network
 
 #######################################################################################
 from app import app
 from common.pereferences import DEBUG, PORT, HOST, THREADED
 
+g = Network()
+# g.add_nodes([1,2,3], value=[10, 100, 400],
+#                          title=['I am node 1', 'node 2 here', 'and im node 3'],
+#                          x=[21.4, 54.2, 11.2],
+#                          y=[100.2, 23.54, 32.1],
+#                          label=['NODE 1', 'NODE 2', 'NODE 3'],
+#                          color=['#00ff1e', '#162347', '#dd4b39'])
+g.save_graph('server/app/templates/nx.html')
+
+
+@app.route('/index')
 @app.route('/')
 def hello():
     user = 'Egor'
@@ -28,6 +40,37 @@ def test():
         data = request.args
 
         return ''
+
+@app.route('/add_node', methods=['POST', 'GET'])
+def add_node():
+        print(request.form)
+        name = request.form['firstname/']
+        surname = request.form['surname']
+        patro = request.form['patro']
+
+        g.add_node(len(g.nodes), label=name, title = f"{surname}/{patro}")
+        g.save_graph('server/app/templates/nx.html')
+
+
+
+        return redirect(url_for('hello'))
+
+
+@app.route('/add_edge', methods=['POST', 'GET'])
+def add_edge():
+        print(request.form)
+        firstid = request.form['firstid']
+        secondid = request.form['secondid']
+        type_data = request.form['type']
+
+        print( g.nodes)
+
+        g.add_edge(int(firstid), int(secondid), type_data = type_data)
+        g.save_graph('server/app/templates/nx.html')
+
+
+
+        return redirect(url_for('hello'))
 
 def main():
     app.run(HOST, PORT, debug=DEBUG, threaded=THREADED)
